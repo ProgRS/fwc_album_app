b025d4b457d81cfd67fc4b3b8d6baf81cecefa43
ProgRs <lfernandotc@hotmail.com>
2022-10-11 15:37:00 -0300

    Initial commit

---
+++ lib/app/core/theme/theme_config.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ThemeConfig{
+  ThemeConfig._();
+
+   static final theme = ThemeData(
+          appBarTheme: const AppBarTheme(
+             backgroundColor: Colors.white
+          )
+   );
+
+}
