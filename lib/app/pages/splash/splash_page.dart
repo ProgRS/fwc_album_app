e56faaca2b231d9a52d6fc1480454ad804531adc
ProgRs <lfernandotc@hotmail.com>
2022-10-13 10:28:45 -0300

    Helpers de tela(Mensagens e loader)

--- lib/app/pages/splash/splash_page.dart
+++ lib/app/pages/splash/splash_page.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:fwc_album_app/app/core/ui/helpers/loader.dart';
+import 'package:fwc_album_app/app/core/ui/helpers/messages.dart';
 import 'package:fwc_album_app/app/core/ui/styles/button_styles.dart';
 import 'package:fwc_album_app/app/core/ui/styles/colors_app.dart';
 import 'package:fwc_album_app/app/core/ui/styles/text_styles.dart';
 import 'package:fwc_album_app/app/core/ui/widgets/button.dart';
 import 'package:fwc_album_app/app/core/ui/widgets/rounded_button.dart';
-class SplashPage extends StatelessWidget {
+class SplashPage extends StatefulWidget {
   const SplashPage({super.key});
 
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> with Loader<SplashPage>, Messages<SplashPage> {
   @override
   Widget build(BuildContext context) {
 
@@ -18,27 +25,35 @@ class SplashPage extends StatelessWidget {
       child: Column(
         children: [
           ElevatedButton(
-          onPressed: (){},
+          onPressed: () async {
+            showLoader();
+           await Future.delayed(Duration(seconds: 2));
+           hideLoader();
+          },
           style: ButtonStyles.instance.primaryButton,
           child: Text('Salvar'),
     ),
           OutlinedButton(
-            onPressed: (){},
+            onPressed: (){
+                showError('Erro no botão outline');
+            },
             style: ButtonStyles.instance.primaryOutlineButton,
             child: Text('Salvar'),
-
           ),
           TextField(),
           Button(
-             onPressed: (){},
+             onPressed: (){
+               showInfo('Info no botão outline');
+             },
               style: ButtonStyles.instance.yellowButton,
               labelStyle: context.textStyles.textPrimaryFontBold,
               label: 'Salvar'),
           Button.primary(
               width: MediaQuery.of(context).size.width * .9,
               height: 30,
-              onPressed: (){},
-
+              onPressed: (){
+                    showSuccess('Success no botão Outline');
+              },
               label: 'Salvar'),
               RoundedButton(
                   icon: Icons.add,
